c4cc7cbf6439d0d0219b907a553e512928e07144
mapfap <mapfaps@gmail.com>
2018-06-10 20:09:19 +0700

    Add more Openshift variables

--- src/config/vars.js
+++ src/config/vars.js
@@ -9,7 +9,7 @@ const publicKey = fs.readFileSync(path.join(__dirname, './public_key.txt'), 'utf
 
 const vars = {
   env: process.env.NODE_ENV,
-  ip: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+  ip: process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
   port:  process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080,
   trustedPublicKey: publicKey,
   mongo: {
@@ -33,7 +33,39 @@ const mask = (text) => text ? 'x'.repeat(text.length) : text;
 logger.info(`MONGO_URI_TESTS: ${mask(process.env.MONGO_URI_TESTS)}`);
 logger.info(`OPENSHIFT_MONGODB_DB_URL: ${mask(process.env.OPENSHIFT_MONGODB_DB_URL)}`);
 logger.info(`MONGO_URI: ${mask(process.env.MONGO_URI)}`);
-logger.info(`------------------------------------`);
+logger.info(`DATABASE_SERVICE_NAME: ${mask(process.env.DATABASE_SERVICE_NAME)}`);
 
+// For Openshift Variables
+if (vars.mongo.uri == null && process.env.DATABASE_SERVICE_NAME) {
+  logger.info(`${mongoServiceName + '_SERVICE_HOST'}: ${process.env[mongoServiceName + '_SERVICE_HOST']}`);
+  logger.info(`${mongoServiceName + '_SERVICE_PORT'}: ${process.env[mongoServiceName + '_SERVICE_PORT']}`);
+  logger.info(`${mongoServiceName + '_DATABASE'}: ${process.env[mongoServiceName + '_DATABASE']}`);
+  logger.info(`${mongoServiceName + '_PASSWORD'}: ${mask(process.env[mongoServiceName + '_PASSWORD'])}`);
+  logger.info(`${mongoServiceName + '_USER'}: ${process.env[mongoServiceName + '_USER']}`);
+
+  const mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+  const mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'];
+  const mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'];
+  const mongoDatabase = process.env[mongoServiceName + '_DATABASE'];
+  const mongoPassword = process.env[mongoServiceName + '_PASSWORD'];
+  const mongoUser = process.env[mongoServiceName + '_USER'];
+
+  let mongoUrlLabel = '';
+  let mongoUrl = '';
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    
+    mongoUrlLabel = mongoUrl = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoUrl += mongoUser + ':' + mongoPassword + '@';
+    }
+    mongoUrlLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoUrl += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+    vars.mongo.uri = mongoUrl;
+    logger.info(`MongoDB connection: ${mongoUrlLabel}`);
+  }
+}
+
+logger.info(`------------------------------------`);
 
 module.exports = vars;
